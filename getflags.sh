26f86d63cdac63b2b9fea2adef16b48a3205f2ea
koraynilay <koray.fra@gmail.com>
2024-04-19 12:46:30 +0200

    2024-04-19_12-46-30

--- getflags.sh
+++ getflags.sh
@@ -2,11 +2,13 @@
 [[ "$#" -lt 2 ]] && echo -e "Usage: $0 [service name] [script executable]\nOR\nUsage: $0 [service name] [script executable] [team ip]" && exit 1
 service=$1
 team=$3
+dir="/tmp"
 if [ "$#" -eq 2 ];then
-	filename="flagids_${service}.json"
+	filename="${dir}/flagids_${service}.json"
 	curl http://10.10.0.1:8081/flagIds\?service\=$service > "$filename"
 elif [ "$#" -eq 3 ];then
-	filename="flagids_${service}_${team}.json"
+	filename="${dir}/flagids_${service}_${team}.json"
 	curl http://10.10.0.1:8081/flagIds\?service\=$service\&team\=$team > "$filename"
 fi
 ./"$2" "$filename"
+rm -v "$filename"
