47cb74ad219244450f6ca3bbd9c9c766211ed67b
koraynilay <koray.fra@gmail.com>
2024-04-19 20:57:04 +0200

    2024-04-19_20-57-04

--- getflags.sh
+++ getflags.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-[[ "$#" -lt 2 ]] && \
-	echo -e "Usage: $0 [script executable] [service name]" \
-	echo "OR" \
-	echo "Usage: $0 [team ip] [script executable] [service name]" \
+[[ "$#" -lt 2 ]] \
+	&& echo -e "Usage: $0 [service name] [script executable]" \
+	&& echo "OR" \
+	&& echo "Usage: $0 [service name] [script executable] [team ip]" \
 	&& exit 1
-team=$1
-service=$3
+team=$3
+service=$1
 dir="/tmp"
 if [ "$#" -eq 2 ];then
 	filename="${dir}/flagids_${service}.json"
