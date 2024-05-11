1c7243271bb99a42eacbd26b47a3061f249a4e15
koraynilay <koray.fra@gmail.com>
2024-05-11 03:11:07 +0200

    add firegex password as arg to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 simad_folder="$HOME/simad"
 
 # install and start firegex
-"$simad_folder"/firegex.py --port 65000
+if [ -n "$1" ];then
+	"$simad_folder"/firegex.py --port 65000 --startup-psw "$1"
+else
+	"$simad_folder"/firegex.py --port 65000
+fi
 # uncomment this if you want to get the latest version
 #sh <(curl -sLf https://pwnzer0tt1.it/firegex.sh) --port 65000
 
