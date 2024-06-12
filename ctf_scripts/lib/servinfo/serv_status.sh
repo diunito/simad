cbebb4c5b3e5deacba16e6ae1989d43d90e69a4d
koraynilay <koray.fra@gmail.com>
2024-06-12 23:02:51 +0200

    serv_status.sh: switch vars to local, add example comments

--- ctf_scripts/lib/servinfo/serv_status.sh
+++ ctf_scripts/lib/servinfo/serv_status.sh
@@ -3,6 +3,13 @@ check_web() {
 	# $2: ip
 	# $3: port
 	curl http://$2:$3 --write-out "%{http_code}" --silent --output /dev/null
+	local ret=$?
+	echo -n " $ret "
+	if [ "$ret" = "200" ];then
+		return 0
+	else
+		return 1
+	fi
 }
 check_tcp() {
 	# $2: ip
@@ -21,12 +28,17 @@ check_serv() {
 }
 
 check_single_service() {
-	serv_type="$1"
-	ip="$2"
-	port="$3"
-	serv_name="$4"
+	local serv_type="$1"
+	local ip="$2"
+	local port="$3"
+	local serv_name="$4"
+	local container_name="$4"
 
-	echo -n "testing type $serv_type\t$ip:$port ($serv_name)... " 
+	# example output:
+	# testing type web 10.60.81.1:80 fimlibrary (nginx)... OK
+	# testing type tcp 10.60.81.1:1337 rpn (rpn)... FAILED
+	# testing type sql 10.60.81.1:3306 filmlibrary (filmlibrary_sql)... FAILED
+	echo -en "testing type $serv_type\t$ip:$port $serv_name ($container_name)... "
 	if ! check_serv $serv_type $ip $port;then
 		echo "FAILED"
 	else
