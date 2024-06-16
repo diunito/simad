08ce81bbd7c565eb96677206ddc0a655ad29cbdb
koraynilay <koray.fra@gmail.com>
2024-06-17 01:51:35 +0200

    deploy_clone.sh now uses deploy.sh; update firegex start in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 simad_folder="$HOME/simad"
 
 # install and start firegex
-if [ -n "$1" ];then
-	"$simad_folder"/firegex.py --port 65000 --psw-no-interactive "$1"
-else
-	"$simad_folder"/firegex.py --port 65000
-fi
-# uncomment this if you want to get the latest version
-#sh <(curl -sLf https://pwnzer0tt1.it/firegex.sh) --port 65000
+"$simad_folder/firegex/start.py" --port 65000 $(if [ -n "$1" ];then echo --psw-no-interactive "$1";fi)
+# use this instead if you want to get the upstream version
+#sh <(curl -sLf https://pwnzer0tt1.it/firegex.sh) --port 65000 $(if [ -n "$1" ];then echo --psw-no-interactive "$1";fi)
 
 # start tcpdump
 "$simad_folder"/ctf_scripts/vuln/dump.sh /pcaps
--- deploy_clone.sh
+++ deploy_clone.sh
@@ -4,10 +4,5 @@ simad_folder="$HOME/simad"
 # clone simad repo in $HOME (should be /root)
 git clone --recurse-submodules https://github.com/koraynilay/simad "$simad_folder"
 
-# install and start firegex
-"$simad_folder"/firegex.py --port 65000
-# uncomment this if you want to get the latest version
-#sh <(curl -sLf https://pwnzer0tt1.it/firegex.sh) --port 65000
-
-# start tcpdump
-"$simad_folder"/ctf_scripts/vuln/dump.sh /pcaps
+cd "$simad_folder" || echo "cd $simad_folder failed" && return
+./deploy.sh "$@"
