416df7ce2c3612bb543416acb7ce3e0e3109ec0f
koraynilay <koray.fra@gmail.com>
2024-06-21 01:39:46 +0200

    export .env for DestructiveFarm before starting it

--- start_df.sh
+++ start_df.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -a # set allexport so that the sourced .env vars are exported
+. priv/DestructiveFarm/.env
 DestructiveFarm/server/start_server.sh
+set +a # unset allexport
